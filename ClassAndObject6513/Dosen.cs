70fe46aa46e7f7108f9db8320743224ea9ec4c59
Rizkirhmd8 <akimrhmd@gmail.com>
2026-04-14 14:57:47 +0700

    Add files via upload

---
+++ ClassAndObject6513/Dosen.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAndObject6513
+{
+    internal class Dosen
+    {
+        //atribut
+        public string nik { get; set; }
+        public string nama {get; set;}
+
+        //behavior
+        public void Mengajar()
+        {
+            Console.WriteLine("{0} mengajar Kalkulus", nama);
+        }
+
+        public void Menulis()
+        {
+            Console.WriteLine("{0} menulis riset", nama);
+        }
+    }
+}

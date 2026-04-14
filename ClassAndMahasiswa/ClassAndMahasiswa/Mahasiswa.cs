dcb284d9e38b94bb3b5de42c6683b0851de7340b
Rizkirhmd8 <akimrhmd@gmail.com>
2026-04-14 14:58:19 +0700

    Add files via upload

---
+++ ClassAndMahasiswa/ClassAndMahasiswa/Mahasiswa.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAndMahasiswa
+{
+    internal class Mahasiswa
+    {
+        //properties
+        public string nim { get; set; }
+        public string nama { get; set; }
+        public string ipk { get; set; }
+
+        //method
+        public void Registrasi()
+        {
+            Console.WriteLine("Nama : {0}", nama);
+            Console.WriteLine("NIM : {0}", nim);
+            Console.WriteLine("IPK : {0}", ipk);
+            Console.WriteLine("\nTelah Melakukan registrasi");
+        }
+        public void IsiKrs()
+        {
+            Console.WriteLine("{0} sedang melakukan proses Krs", nama);
+        }
+
+    }
+}

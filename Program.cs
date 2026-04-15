1d0780bc1e650265ce42d8eb0a1f5c603c8affc4
Akimrhmd8 <akimrhmd@gmail.com>
2026-04-15 19:40:35 +0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Mahasiswa_6513
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //membuat object class Mahasiswa
+            Mahasiswa mahasiswa1 = new Mahasiswa();
+            Mahasiswa mahasiswa2 = new Mahasiswa();
+
+            //mengeset nilai properties object saya dan kamu
+            mahasiswa1.Nim = "6513";
+            mahasiswa1.Nama = "Rizki Rahmadi";
+            mahasiswa1.Ipk = 3.95f;
+
+            mahasiswa2.Nim = "2522";
+            mahasiswa2.Nama = "Rizki Tanjung";
+            mahasiswa2.Ipk = 2.80f;
+
+            //memangil method register dan isi krs
+            mahasiswa1.Registrasi();
+            mahasiswa1.IsiKrs();
+
+            mahasiswa2.Registrasi();
+            mahasiswa2.IsiKrs();
+
+            Console.ReadKey();
+        }
+    }
+}
